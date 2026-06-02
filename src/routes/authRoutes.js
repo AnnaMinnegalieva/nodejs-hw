2db0173b5a49905e622799ff26f8c5d87fc972fb
AnnaMinnegalieva <minnegalieva.anna@gmail.com>
2026-06-02 17:53:49 +0300

    module-5

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,3 @@
-
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
 import {
@@ -6,10 +5,14 @@ import {
   logoutUser,
   refreshUserSession,
   registerUser,
+  requestResetEmail,
+  resetPassword,
 } from '../controllers/authController.js';
 import {
   loginUserSchema,
   registerUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
 } from '../validations/authValidation.js';
 
 const router = Router();
@@ -18,5 +21,17 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
+
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
+
+router.post(
+  '/auth/reset-password',
+  celebrate(resetPasswordSchema),
+  resetPassword,
+);
 
 export default router;
--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,4 +1,3 @@
-
 import { Joi, Segments } from 'celebrate';
 
 export const registerUserSchema = {
@@ -12,5 +11,18 @@ export const loginUserSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
+  }),
+};
+
+export const requestResetEmailSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
+  }),
+};
+
+export const resetPasswordSchema = {
+  [Segments.BODY]: Joi.object({
+    password: Joi.string().min(8).required(),
+    token: Joi.string().required(),
   }),
 };
